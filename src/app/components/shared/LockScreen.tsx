7c72d95d2047ce40559a318fcf446eda66b2a670
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2025-10-31 01:22:08 +0530

    changed the lock screen a bit

--- src/app/components/shared/LockScreen.tsx
+++ src/app/components/shared/LockScreen.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { BugAntIcon } from "@heroicons/react/24/outline"
 import { motion } from "framer-motion"
 import { Pixelify_Sans } from "next/font/google"
 import { useState } from "react"
@@ -32,7 +33,11 @@ export default function LockScreen({
                 exit={{ opacity: 0, y: -10 }}
                 transition={{ duration: 0.1, ease: 'easeInOut' }}
                 className="mt-16 w-full flex justify-center items-center -ml-12 font-product z-1"
-            > <div className="flex flex-col">
+            >
+                <BugAntIcon
+                    className="w-24 text-gray-300 rotate-45 mr-44 -mt-44 z-2"
+                />
+                <div className="z-1 flex flex-col">
                     <motion.div
                         initial={{ opacity: 0, y: -10 }}
                         animate={{ opacity: 1, y: 0 }}
@@ -42,15 +47,16 @@ export default function LockScreen({
                     >
                         <p className={`${pixel.className} text-4xl`}> Protected folder </p>
                         <form
-                            className="flex items-center space-x-4 mt-16"
+                            className="flex items-center space-x-6 mt-16"
                             onSubmit={(e) => verify(e)}
                         >
                             <div className="flex flex-col space-y-2">
                                 <input
                                     type="password"
                                     onChange={(e) => setTypedPassword(e.target.value)}
-                                    className="w-[16rem] rounded-md p-2 outline-none border-2 border-gray-400"
+                                    className="w-[16rem] h-[3rem] rounded-md p-4 outline-none border border-gray-400 focus:shadow-md placeholder:text-gray-400 placeholder:text-sm placeholder:italic"
                                     autoFocus={true}
+                                    placeholder="type the truth...."
                                 />
                                 {
                                     error && <p className="text-sm text-red-400"> {error} </p>
@@ -61,13 +67,13 @@ export default function LockScreen({
                                 animate={{ opacity: 1, y: 0 }}
                                 exit={{ opacity: 0, y: -20 }}
                                 transition={{ duration: 0.2 }}
-                                className="bg-gray-400 text-white hover text-sm rounded-md p-2"
+                                className="transition bg-white text-gray-600 hover:bg-gray-600 hover:text-white hover text-sm w-10 h-10 rounded-sm border border-black p-2"
                                 onClick={() => verify}
                             >
-                                Lets goo
+                                Go
                             </motion.button>
                         </form>
-                        <div className="bg-gray-400 p-4 rounded-md flex items-center space-y-2 text-white mt-20">
+                        <div className="bg-gray-100 p-4 rounded-md flex items-center space-y-2 text-gray-400 mt-32">
                             <ul>
                                 <li> the owner of the file has protected this file with a password </li>
                                 <li> type the password of this folder to access its items </li>
@@ -75,6 +81,9 @@ export default function LockScreen({
                         </div>
                     </motion.div>
                 </div>
+                <BugAntIcon
+                    className="w-24 text-gray-300 rotate-225 ml-44 mt-44"
+                />
             </motion.div>
         </>
     )
